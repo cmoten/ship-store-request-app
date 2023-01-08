4506bcda13150b721e1b70a348944d1f4d57143d
cmoten <cmoten@gmail.com>
2023-01-08 20:18:50 +0100

    Updated class libraries for the core models

---
+++ ShipRequestApp/ShipRequestApp.Core/Model/OrderByUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipRequestApp.Core.Model
+{
+    public class OrderByUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BuildingNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+        public DateTime OrderPlaced
+        {
+            get; set;
+        }
+    }
+}
