93aaecd071d848f8d4f081df4e53ad5a7de6eede
cmoten <cmoten@gmail.com>
2023-01-08 13:01:19 +0100

    Updated main window

--- ship-request-app/ship-request-app/MainWindow.xaml.cs
+++ ship-request-app/ship-request-app/MainWindow.xaml.cs
@@ -15,25 +15,21 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace ship_request_app
-{
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-        }
+namespace ship_request_app;
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : WindowEx
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
     }
+
 }
